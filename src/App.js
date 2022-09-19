b6322980cdf87bdba5c7e90251695c766e5deefa
molim <mohade3.karbalaei@gmail.com>
2022-09-19 18:32:12 +0200

    add TooltipComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import React from "react";
-import "./index.css";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import "./App.css";
 
 const App = () => {
-  return <div className="underline text-3xl">App</div>;
+  const activeMenu = true;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            {/****1-**** we use tooltip component here:
+           we use it becuse we want to show tooltip when user hover on the icon: */}
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                // we want to change the style based on the theme:
+                style={{ backgroundColor: "#1a202c", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+
+          {/* ****2-**** go for sidebar: */}
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">sidebar w-0</div>
+          )}
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
 
 // we install: $ npm instal --legacy--peer-peps
+
+//1- creating tooltip component for hover on the icon:
