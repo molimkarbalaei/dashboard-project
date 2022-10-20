ff326680281dff1c906abde521e718d720cd2d1f
molim <mohade3.karbalaei@gmail.com>
2022-10-20 11:34:13 +0200

    add styling and props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+// we pass all the props we accept in ecommerce.jsx
+const Button = ({ bgColor, color, borderRadius, text, size }) => {
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
