65c885b061c9cd4d1323d581cc20fc313f730609
molim <mohade3.karbalaei@gmail.com>
2022-10-20 13:56:43 +0200

    add: revenue + carts

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -17,7 +17,112 @@ const Ecommerce = () => {
           className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44
         rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center"
         >
-          hvhg
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="font-bold text-gray-400 ">Earnings</p>
+              <p className="text-2xl">$63,448.78</p>
+            </div>
+          </div>
+          <div className="mt-6 ">
+            {/* add some props to the button: */}
+            <Button
+              color="white"
+              bgColor="blue"
+              text="Download"
+              borderRadius="10px"
+              size="md"
+            />
+          </div>
+        </div>
+        {/* the cards: */}
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center ">
+          {/* map the cards: */}
+          {earningData.map((item) => (
+            <div
+              key={item.key}
+              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
+            >
+              <button
+                type="button"
+                style={{
+                  color: item.iconColor,
+                  backgroundColor: item.iconBg,
+                }}
+                className="text-2xl hover:drop-shadow-xl opacity-0.9 rounded-full p-4 "
+              >
+                {item.icon}
+              </button>
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className="text-sm text-gray-400 mt-1 ">{item.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Revenue Updates: */}
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:bg-secondary-dark-bg dark:text-gray-200 rounded-2xl m-3 p-4 md:w-780 ">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+          <div className="mt-10 flex gap-10 justify-center flex-wrap">
+            <div className="border-r-1 m-4 pr-10 border-color ">
+              <div>
+                <p>
+                  <span className="text-3xl font-semibold">$93,438</span>
+                  <span className="p-1.5 cursor-pointer hover:drop-shadow-xl rounded-full bg-green-400 ml-3 text-white text-sm">
+                    23%
+                  </span>
+                </p>
+                <p className="text-gray-400 mt-1">Budget</p>
+              </div>
+              <div className="mt-8">
+                <p>
+                  <span className="text-3xl font-semibold">$48,487</span>
+                </p>
+                <p className="text-gray-400 mt-1">Expense</p>
+              </div>
+              {/* sparkline chart: */}
+              <div className="mt-5">
+                <SparkLine
+                  currentColor="blue"
+                  id="line-sparkline"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                />
+              </div>
+              {/* button for downloads: */}
+              <div className="mt-10 ">
+                <Button
+                  color="white"
+                  text="Download Report"
+                  bgColor="blue"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
