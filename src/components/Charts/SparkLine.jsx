4883be963a071c9f1a6c48d37c007db9455e8553
molim <mohade3.karbalaei@gmail.com>
2022-10-20 13:52:48 +0200

    add sparkline data

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+// after implementing ecommerce charts we have to put;
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const SparkLine = () => {
-  return <div>SparkLine</div>;
+// pasing all props:
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      type={type}
+      xName="x"
+      yName="yval"
+      // more props:
+      tooltipSettings={{
+        visible: true,
+        format: "${x} : data ${yval}",
+        // that line is for tooltip:
+        trackLineSettings: {
+          visible: true,
+        },
+      }}
+      // for bolet points:
+      markerSettings={{
+        visible: ["All"],
+      }}
+    >
+      {/* based on documentation: */}
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
 };
 
 export default SparkLine;
