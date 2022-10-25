70dd1c44fb840cbfd9f6da49bacca12400939fa7
molim <mohade3.karbalaei@gmail.com>
2022-10-25 13:16:13 +0200

    feat: add props to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+//  after adding header in order page we add header in here:
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ category, title }) => {
+  // we call props from order page:
+  return (
+    <div className="mb-10">
+      <p className="text-gray-400">{category}</p>
+      <p className="text-3xl font-extrabold tracking-tight text-slate-900">
+        {title}
+      </p>
+    </div>
+  );
 };
 
 export default Header;
