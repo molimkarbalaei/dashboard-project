32dda158e42d1cf3643a616d59b04708d14745aa
molim <mohade3.karbalaei@gmail.com>
2022-10-25 13:29:17 +0200

    feat: add order page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+// after adding ecommerce page we go to orders page:
+// we do: sort by name, customer name, the amount, status and order id.
+
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Resize,
+  Sort,
+  Filter,
+  Page,
+  Inject,
+  Edit,
+  PdfExport,
+  ExcelExport,
+  ContextMenu,
+} from "@syncfusion/ej2-react-grids";
+
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      {/*  we use syncfusion for implementing the grid: */}
+    </div>
+  );
 };
 
 export default Orders;
