43ce4f33611c8dcba1422e365f3fbc434e5a01bf
molim <mohade3.karbalaei@gmail.com>
2022-10-31 12:57:19 +0100

    feat: add kanban

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-kanban";
+
+import { kanbanData, kanbanGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const kanban = () => {
-  return <div>kanban</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Kanban" />
+      {/* import the actual data: */}
+      <KanbanComponent
+        id="kanban"
+        dataSource={kanbanData}
+        keyField="Status"
+        cardSettings={{ contentField: "Summary", headerField: "Id" }}
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((item, index) => (
+            // we have to provide key for each single data
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
+  );
 };
 
 export default kanban;
