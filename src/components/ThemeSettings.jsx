d1a38549535d52dc835445f350308a0cc9e52cc6
molim <mohade3.karbalaei@gmail.com>
2022-11-03 12:10:55 +0100

    edit

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -16,7 +16,7 @@ const ThemeSettings = () => {
   // first create A layout for the theme settings:
   return (
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
-      <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400">
+      <div className="float-right h-screen dark:text-gray-400 bg-white dark:[#484B52] w-400">
         {/* sidebar for themesettings: */}
         <div className="flex justify-between p-4 ml-4 items-center">
           <p className="font-semibold text-xl"> Settings</p>
